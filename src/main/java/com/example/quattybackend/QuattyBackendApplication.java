6e5ec2d309f6b7adb0bee330eb10de56fd638533
Erickson Azevedo <erickson.tulio96@gmail.com>
2021-09-26 00:48:22 -0300

    backend funcionando com docker

---
+++ src/main/java/com/example/quattybackend/QuattyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quattybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuattyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuattyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quattybackend/QuattyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quattybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuattyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
